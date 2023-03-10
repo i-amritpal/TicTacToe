38c07c428eb07729eefe06dff6b8e333254a6cd9
Amritpal Singh <er.amritpal17@gmail.com>
2023-03-10 14:01:15 +0530

    Displaying Winner

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,36 +20,113 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String displayExOh = '';
+  bool ohTurn = true; //The first player is oh
+  List<String> displayExOh = ['', '', '', '', '', '', '', '', ''];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
       body: GridView.builder(
           itemCount: 9,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3), itemBuilder: (BuildContext_context, int index){
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemBuilder: (BuildContext_context, int index) {
             return GestureDetector(
-              onTap: _tapped,
+              onTap: () {
+                _tapped(index);
+              },
               child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey)
-                ),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.grey)),
                 child: Center(
-                  child: Text(displayExOh, style: TextStyle(color: Colors.white, fontSize: 40),),
+                  child: Text(
+                    displayExOh[index],
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
                 ),
               ),
             );
-      }),
-
+          }),
     );
   }
 
-  void _tapped(){
+  void _tapped(int index) {
     setState(() {
-      displayExOh= '0';
+      if (ohTurn) {
+        displayExOh[index] = 'O';
+      } else {
+        displayExOh[index] = 'x';
+      }
+      ohTurn = !ohTurn;
+      _checkWinnner();
     });
   }
 
+  void _checkWinnner() {
+    //checks 1st row
+    if (displayExOh[0] == displayExOh[1] &&
+        displayExOh[0] == displayExOh[2] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    }
 
+    //checks 2nd row
+    if (displayExOh[3] == displayExOh[4] &&
+        displayExOh[3] == displayExOh[5] &&
+        displayExOh[3] != '') {
+      _showWinDialog(displayExOh[3]);
+    }
 
+    //checks 3nd row
+    if (displayExOh[6] == displayExOh[7] &&
+        displayExOh[6] == displayExOh[8] &&
+        displayExOh[6] != '') {
+      _showWinDialog(displayExOh[6]);
+    }
+
+    //checks 1st column
+    if (displayExOh[0] == displayExOh[3] &&
+        displayExOh[0] == displayExOh[6] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    }
+
+    //checks 2nd column
+    if (displayExOh[1] == displayExOh[4] &&
+        displayExOh[1] == displayExOh[7] &&
+        displayExOh[1] != '') {
+      _showWinDialog(displayExOh[1]);
+    }
+
+    //checks 3rd column
+    if (displayExOh[2] == displayExOh[5] &&
+        displayExOh[2] == displayExOh[8] &&
+        displayExOh[2] != '') {
+      _showWinDialog(displayExOh[2]);
+    }
+
+    //checks diagonal
+    if (displayExOh[0] == displayExOh[4] &&
+        displayExOh[0] == displayExOh[8] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    }
+
+    //checks diagonal
+    if (displayExOh[2] == displayExOh[4] &&
+        displayExOh[2] == displayExOh[6] &&
+        displayExOh[2] != '') {
+      _showWinDialog(displayExOh[2]);
+    }
+  }
+
+  void _showWinDialog(String winner) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('WINNER is: '+ winner),
+          );
+        });
+  }
 }
